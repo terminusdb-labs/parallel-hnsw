fe79547d4efa3b79f05c562bbffebd6599961d5d
Matthijs van Otterdijk <matthijs@terminusdb.com>
2023-12-14 14:40:16 +0100

    an attempt at neighborhood construction

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,38 @@
-#[derive(PartialEq, Eq, Debug, PartialOrd, Ord, Clone, Copy)]
-struct VectorId(usize);
-#[derive(PartialEq, Eq, Debug, PartialOrd, Ord, Clone, Copy)]
-struct NodeId(usize);
+use std::{cell::UnsafeCell, collections::HashSet, marker::PhantomData};
 
-pub struct Layer<const M: usize> {
-    metric: Box<Fn(VectorId, VectorId) -> f32>,
-    nodes: Vec<VectorId>,
-    neighbours: Vec<NodeId>,
+use rand::{thread_rng, Rng};
+use rayon::prelude::*;
+
+#[derive(PartialEq, Eq, Debug, PartialOrd, Ord, Clone, Copy)]
+pub struct VectorId(usize);
+#[derive(PartialEq, Eq, Debug, PartialOrd, Ord, Clone, Copy)]
+pub struct NodeId(usize);
+
+pub trait Comparator<T>: Sync {
+    fn compare_stored(&self, v1: VectorId, v2: VectorId) -> f32;
+    fn compare_half_stored(&self, v1: VectorId, v2: &T) -> f32;
+    fn compare_unstored(&self, v1: &T, v2: &T) -> f32;
 }
 
-impl<const M: usize> Layer<M> {
+#[derive(PartialEq, PartialOrd, Clone, Copy, Debug)]
+pub struct OrderedFloat(f32);
+
+impl Eq for OrderedFloat {}
+
+impl Ord for OrderedFloat {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
+
+pub struct Layer<const NeighborhoodSize: usize, C: Comparator<T>, T> {
+    comparator: C,
+    nodes: Vec<VectorId>,
+    neighbors: Vec<NodeId>,
+    _phantom: PhantomData<T>,
+}
+
+impl<const NeighborhoodSize: usize, C: Comparator<T>, T> Layer<NeighborhoodSize, C, T> {
     fn get_node(&self, v: VectorId) -> NodeId {
         NodeId(self.nodes.binary_search(&v).unwrap())
     }
@@ -19,12 +42,69 @@ impl<const M: usize> Layer<M> {
     }
 
     fn get_neighbours(&self, n: NodeId) -> &[NodeId] {
-        &self.neighbours[(n.0 * M)..((n.0 + 1) * M)]
+        &self.neighbors[(n.0 * NeighborhoodSize)..((n.0 + 1) * NeighborhoodSize)]
     }
 
-    fn generate(vs: Vec<VectorId>) -> Self {
-        //
+    pub fn generate(comparator: C, vs: Vec<VectorId>) -> Self {
+        let max = vs.len();
+
+        let mut all_distances: Vec<_> = vs
+            .par_iter()
+            .map(|id| {
+                let choices = choose_n(NeighborhoodSize * 10, vs.len(), id.0);
+                let mut distances = vec![(0, 0.0); NeighborhoodSize * 10];
+                for i in 0..choices.len() {
+                    let choice = choices[i];
+                    let distance = comparator.compare_stored(*id, VectorId(choice));
+                    distances[i] = (choice, distance);
+                }
+                distances.sort_by_key(|d| OrderedFloat(d.1));
+                distances.truncate(NeighborhoodSize);
+
+                UnsafeCell::new(distances)
+            })
+            .collect();
+
+        for i in 0..all_distances.len() {
+            for (n, d) in unsafe { &*(all_distances[i].get()) } {
+                debug_assert!(*n != i);
+                let other = all_distances[*n].get_mut();
+                other.push((i, *d));
+            }
+        }
+
+        let mut neighbors = vec![NodeId(0); vs.len() * NeighborhoodSize];
+        for i in 0..all_distances.len() {
+            let distances = all_distances[i].get_mut();
+            distances.sort_by_key(|d| OrderedFloat(d.1));
+            distances.dedup();
+            distances.truncate(NeighborhoodSize);
+            for j in 0..distances.len() {
+                neighbors[i * NeighborhoodSize + j] = NodeId(distances[j].0);
+            }
+        }
+
+        Self {
+            comparator,
+            nodes: vs,
+            neighbors,
+            _phantom: PhantomData,
+        }
     }
+}
+
+fn choose_n(n: usize, max: usize, exclude: usize) -> Vec<usize> {
+    let mut rng = thread_rng();
+    let mut count = 0;
+    let mut set = HashSet::with_capacity(n);
+    while count != n {
+        let selection = rng.gen_range(0..max);
+        if selection != exclude && set.insert(selection) {
+            count += 1;
+        }
+    }
+
+    set.into_iter().collect()
 }
 
 #[cfg(test)]
