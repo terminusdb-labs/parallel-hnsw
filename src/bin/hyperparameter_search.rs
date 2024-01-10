ed21bb2bf5de9d8b394d2fac96407516894529e0
Gavin Mendel-Gleason <gavin@terminusdb.com>
2024-01-10 16:49:12 +0100

    get proper options

--- src/bin/hyperparameter_search.rs
+++ src/bin/hyperparameter_search.rs
@@ -48,58 +48,65 @@ fn do_test_recall(
 pub fn main() {
     let vector_size = 1000;
     let input_size = 10000;
-    let neighborhood: Vec<usize> = (10..30).map(|n| n * 2).collect();
-    let candidates: Vec<usize> = (1..10).map(|n| n * 100).collect();
-    let probe_depth: Vec<usize> = (1..5).collect();
-    let supers: Vec<usize> = (1..10).collect();
+    let neighborhood_vec: Vec<usize> = (10..30).map(|n| n * 2).collect();
+    let candidates_vec: Vec<usize> = (1..10).map(|n| n * 100).collect();
+    let probe_depth_vec: Vec<usize> = (1..5).collect();
+    let supers_vec: Vec<usize> = (1..10).collect();
     let mut neighborhood_optimizer = tpe::TpeOptimizer::new(
         tpe::histogram_estimator(),
-        tpe::categorical_range(neighborhood.len()).unwrap(),
+        tpe::categorical_range(neighborhood_vec.len()).unwrap(),
     );
     let mut candidates_optimizer = tpe::TpeOptimizer::new(
         tpe::histogram_estimator(),
-        tpe::categorical_range(candidates.len()).unwrap(),
+        tpe::categorical_range(candidates_vec.len()).unwrap(),
     );
     let mut probe_optimizer = tpe::TpeOptimizer::new(
         tpe::histogram_estimator(),
-        tpe::categorical_range(probe_depth.len()).unwrap(),
+        tpe::categorical_range(probe_depth_vec.len()).unwrap(),
     );
     let mut supers_optimizer = tpe::TpeOptimizer::new(
         tpe::histogram_estimator(),
-        tpe::categorical_range(supers.len()).unwrap(),
+        tpe::categorical_range(supers_vec.len()).unwrap(),
     );
     let mut rng = rand::rngs::StdRng::from_seed(Default::default());
-    let mut best_values = (0.0, 0.0, 0.0, 0.0);
+    let mut best_values = (0, 0, 0, 0);
     let mut best_value = std::f64::INFINITY;
     for _ in 0..100 {
-        let neighborhood = neighborhood_optimizer.ask(&mut rng).unwrap();
-        let candidates = candidates_optimizer.ask(&mut rng).unwrap();
-        let supers = supers_optimizer.ask(&mut rng).unwrap();
-        let probe_depth = probe_optimizer.ask(&mut rng).unwrap();
-        eprintln!("trying neigborhood: {}", neighborhood as usize);
-        eprintln!("trying candidates: {}", candidates as usize);
-        eprintln!("trying supers: {}", supers as usize);
-        eprintln!("trying probe_depth: {}", probe_depth as usize);
-        let hnsw = make_random_hnsw(input_size, vector_size, neighborhood as usize);
-        let total_relevant = do_test_recall(
-            &hnsw,
-            candidates as usize,
-            probe_depth as usize,
-            supers as usize,
-        );
+        let neighborhood_idx = neighborhood_optimizer.ask(&mut rng).unwrap();
+        let neighborhood = neighborhood_vec[neighborhood_idx as usize];
+        let candidates_idx = candidates_optimizer.ask(&mut rng).unwrap();
+        let candidates = candidates_vec[candidates_idx as usize];
+        let supers_idx = supers_optimizer.ask(&mut rng).unwrap();
+        let supers = supers_vec[supers_idx as usize];
+        let probe_depth_idx = probe_optimizer.ask(&mut rng).unwrap();
+        let probe_depth = probe_depth_vec[probe_depth_idx as usize];
+
+        eprintln!("trying neigborhood: {}", neighborhood);
+        eprintln!("trying candidates: {}", candidates);
+        eprintln!("trying supers: {}", supers);
+        eprintln!("trying probe_depth: {}", probe_depth);
+        let hnsw = make_random_hnsw(input_size, vector_size, neighborhood);
+        let total_relevant = do_test_recall(&hnsw, candidates, probe_depth, supers as usize);
         let recall = total_relevant as f64 / input_size as f64;
         eprintln!("Obtained recall of: {recall}");
         let one_minus_recall = 1.0 - recall;
+
+        // tell optimizer
         neighborhood_optimizer
-            .tell(neighborhood, one_minus_recall)
+            .tell(neighborhood_idx, one_minus_recall)
             .unwrap();
         candidates_optimizer
-            .tell(candidates, one_minus_recall)
+            .tell(candidates_idx, one_minus_recall)
             .unwrap();
-        supers_optimizer.tell(supers, one_minus_recall).unwrap();
+        supers_optimizer.tell(supers_idx, one_minus_recall).unwrap();
+        probe_optimizer
+            .tell(probe_depth_idx, one_minus_recall)
+            .unwrap();
+
         if one_minus_recall < best_value {
             best_values = (neighborhood, candidates, probe_depth, supers)
         }
+
         best_value = best_value.min(one_minus_recall);
     }
 
